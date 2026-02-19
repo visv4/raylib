8c00af51d142b4d770735ba09bd265bd068ec4f4
DamirAlkhaov <alkhaovdamirgt@gmail.com>
2026-02-19 21:34:10 +0300

    Forgot to add UpdateEmpties() to raylib.h

--- src/raylib.h
+++ src/raylib.h
@@ -1637,6 +1637,7 @@ RLAPI void SetModelMeshMaterial(Model *model, int meshId, int materialId);      
 // Model animations loading/unloading functions
 RLAPI ModelAnimation *LoadModelAnimations(const char *fileName, int *animCount);            // Load model animations from file
 RLAPI void UpdateModelAnimation(Model model, ModelAnimation anim, int frame);               // Update model animation pose (CPU)
+RLAPI void UpdateEmpties(Model *model, ModelAnimation anim, int frame);                     // Update model empties based on animation.
 RLAPI void UpdateModelAnimationBones(Model model, ModelAnimation anim, int frame);          // Update model animation mesh bone matrices (GPU skinning)
 RLAPI void UnloadModelAnimation(ModelAnimation anim);                                       // Unload animation data
 RLAPI void UnloadModelAnimations(ModelAnimation *animations, int animCount);                // Unload animation array data
